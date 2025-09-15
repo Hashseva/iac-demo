fc6d5d2e1d17f390796f072f9024378e5b921ab7
David Goncalves <95876308+Hashseva@users.noreply.github.com>
2025-09-15 12:43:16 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,9 @@
+variable "ami_id" {
+  description = "AMI factice; change à chaque commit (ex: ami-deadbeef)"
+  type        = string
+  default     = "ami-00000000"
+}
+
 provider "aws" {
   region                      = "us-east-1"
   access_key                  = "test"
@@ -14,3 +20,24 @@ resource "aws_instance" "demo" {
   ami           = "ami-12345678"
   instance_type = "t2.micro"
 }
+
+resource "aws_instance" "demo" {
+  ami           = var.ami_id           # ← varie à chaque commit
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "LocalStackDemo-${substr(var.ami_id, 4, 8)}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.demo.id
+}
+output "image_id" {
+  value = var.ami_id
+}
+
